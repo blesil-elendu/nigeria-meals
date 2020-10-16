9cfa0e0bdb5e5479d1a6f5761ae011537fcac1f5
blessing elendu <blesil86@gmail.com>
2020-10-16 13:18:34 +0100

    Base Commit

---
+++ src/app/views/pages/product/checkout/shipping-details/shipping-details.component.ts
@@ -0,0 +1,70 @@
+import { Product } from "../../../../../shared/models/product";
+import { ShippingService } from "../../../../../shared/services/shipping.service";
+import { UserDetail, User } from "../../../../../shared/models/user";
+import { AuthService } from "../../../../../shared/services/auth.service";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ProductService } from "../../../../../shared/services/product.service";
+import { map } from "rxjs/operators";
+@Component({
+  selector: "app-shipping-details",
+  templateUrl: "./shipping-details.component.html",
+  styleUrls: ["./shipping-details.component.scss"],
+})
+export class ShippingDetailsComponent implements OnInit {
+  userDetails: User;
+
+  userDetail: UserDetail;
+
+  products: Product[];
+
+  constructor(
+    authService: AuthService,
+    private shippingService: ShippingService,
+    productService: ProductService,
+    private router: Router
+  ) {
+    /* Hiding products Element */
+    document.getElementById("productsTab").style.display = "none";
+    document.getElementById("shippingTab").style.display = "block";
+    document.getElementById("productsTab").style.display = "none";
+    document.getElementById("resultTab").style.display = "none";
+
+    this.userDetail = new UserDetail();
+    this.products = productService.getLocalCartProducts();
+    authService.user$.pipe(
+      map((user) => {
+        console.log(user);
+        this.userDetails = user;
+      })
+    );
+  }
+
+  ngOnInit() {}
+
+  updateUserDetails(form: NgForm) {
+    const products = [];
+    let totalPrice = 0;
+    this.products.forEach((product) => {
+      delete product.$key;
+      totalPrice += product.productPrice;
+      products.push(product);
+    });
+    const data = {
+      ...form.value,
+      emailId: this.userDetails.emailId,
+      userId: this.userDetails.$key,
+      products,
+      totalPrice,
+      shippingDate: Date.now(),
+    };
+
+    this.shippingService.createshippings(data);
+
+    this.router.navigate([
+      "checkouts",
+      { outlets: { checkOutlet: ["billing-details"] } },
+    ]);
+  }
+}
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyCWjNGOjcwsUAJNioVBYEWmL6Mky3ndku4",
+  authDomain: "nigeria-meals.firebaseapp.com",
+  databaseURL: "https://nigeria-meals.firebaseio.com",
+  projectId: "nigeria-meals",
+  storageBucket: "nigeria-meals.appspot.com",
+  messagingSenderId: "259248217625",
+  appId: "1:259248217625:web:465edffedaa0c9ab401488",
+  measurementId: "G-V60LPTLC5F"
+};
